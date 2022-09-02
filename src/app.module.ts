025d515bff3a5b5bc669c2901123a143d51434f8
AlbaSly <arias.brando.raxel@gmail.com>
2022-09-01 21:05:30 -0500

    Implement environment configuration into app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,17 +3,20 @@ import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { ConfigModule } from '@nestjs/config';
+
+import { EnvConfiguration } from './config/env.config';
+import { JoiValidationSchema } from './config/joi.validation';
 
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
-import { ConfigModule } from '@nestjs/config';
-import { EnvConfiguration } from './config/env.config';
 //forRoots únicamente se deben hacer en el módulo raíz
 @Module({
   imports: [
     ConfigModule.forRoot({
-      load: [ EnvConfiguration ]
+      load: [ EnvConfiguration ],
+      validationSchema: JoiValidationSchema
     }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
